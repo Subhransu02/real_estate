7ccccaab7dc46295a28089f86da49a7e599aebef
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-13 05:26:36 +0530

    craete signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO)
 })
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
