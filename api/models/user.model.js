cca2d843fc3d250e85d36f22f92f294008cb993f
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-14 01:13:38 +0530

    complete signup page functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    passwords: {
+    password: {
         type: String,
         required: true,
     }
