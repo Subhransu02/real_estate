f5bc8536bb079bf45887b7ad0204d262d12921ce
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-14 04:07:50 +0530

    updated dependencies

--- api/index.js
+++ api/index.js
@@ -3,30 +3,36 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const __dirname = path.resolve();
+
 const app = express();
+
 app.use(express.json());
+
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO)
-.then(() => {
-    console.log('Connected to MongoDB!');
-})
-.catch((err) => {
-    console.log(err);
-})
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
@@ -35,11 +41,11 @@ app.get('*', (req, res) => {
 })
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
+});
